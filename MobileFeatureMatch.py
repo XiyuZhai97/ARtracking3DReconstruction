809a417f22a2be98a918e90c792b0ece7d46b4ff
XiyuZhai <zxy4970@gmail.com>
2019-09-29 02:34:05 -0700

    finished

--- MobileFeatureMatch.py
+++ MobileFeatureMatch.py
@@ -4,6 +4,8 @@ import CalibrationHelpers as calib
 import glob
 import time
 import scipy.io as spio
+import open3d as o3d
+
 relR = spio.loadmat("relR.mat")['R']
 relT = spio.loadmat("relT.mat")['T']
 
@@ -65,12 +67,11 @@ intrinsics, distortion, new_intrinsics, roi = \
         calib.LoadCalibrationData('mobile_calib_data')
 
 imgNum = 0
-inlier_mask_sav = 
+feature_track = np.zeros(len(reference_keypoints)) 
 for fname in images:
     # read the image
     print(fname)
     cap = cv2.imread(fname)
-# while True:
     # read the current frame from the webcam
     current_frame = cv2.resize(cap,(RES,RES))
     
@@ -85,33 +86,133 @@ for fname in images:
     current_keypoints, current_descriptors = \
         feature_detector.detectAndCompute(current_frame, None)
     # match the features from the reference image to the current image
-
+    
     matches = matcher.match(reference_descriptors, current_descriptors)
-    # print("matches", matches)
+    for m in matches:
+        feature_track[m.queryIdx] += 1 
+
+    # referencePoints = np.float32([reference_keypoints[m.queryIdx].pt \
+    #                               for m in matches])
+    # imagePoints = np.float32([current_keypoints[m.trainIdx].pt \
+    #                               for m in matches])
+    # inlier_mask = FilterByEpipolarConstraint(intrinsics, matches, referencePoints, imagePoints, relR[imgNum], relT[imgNum], threshold = 0.01)                 
+    # match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
+    #                     current_keypoints, matches, 0, 
+    #                     matchesMask =inlier_mask, #this applies your inlier filter
+    #                     flags=
+    #                     cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    # cv2.imshow('matches_after filter',match_visualization)
+    # match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
+    #                         current_keypoints, matches, 0, 
+    #                         flags=
+    #                         cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+    # cv2.imshow('matches',match_visualization)
+    # k = cv2.waitKey(1)
+    # # time.sleep(2)
+    # if k == 27 or k==113:  #27, 113 are ascii for escape and q respectively
+    #     #exit
+    #     break
+    imgNum += 1
+print(feature_track)
+cv2.destroyAllWindows()
+imgNum = 0
+
+for fname in images[1:]:
+    # read the image
+    print(fname)
+    cap = cv2.imread(fname)
+    # read the current frame from the webcam
+    current_frame = cv2.resize(cap,(RES,RES))
+    # undistort the current frame using the loaded calibration
+    current_frame = cv2.undistort(current_frame, intrinsics, distortion, None,\
+                                  new_intrinsics)
+    # apply region of interest cropping
+    x, y, w, h = roi
+    current_frame = current_frame[y:y+h, x:x+w]
+    # detect features in the current image
+    current_keypoints, current_descriptors = \
+        feature_detector.detectAndCompute(current_frame, None)
+    # match the features from the reference image to the current image
+    reference_keypoints, reference_descriptors = \
+        feature_detector.detectAndCompute(reference, None)
+    matches = matcher.match(reference_descriptors, current_descriptors)
+    # -Previous
     referencePoints = np.float32([reference_keypoints[m.queryIdx].pt \
                                   for m in matches])
-    
     imagePoints = np.float32([current_keypoints[m.trainIdx].pt \
                                   for m in matches])
-    if(filterflag):
-        
-        inlier_mask = FilterByEpipolarConstraint(intrinsics, matches, referencePoints, imagePoints, relR[imgNum], relT[imgNum], threshold = 0.01)
-                               
-        match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
+    inlier_mask = FilterByEpipolarConstraint(intrinsics, matches, referencePoints, imagePoints, relR[imgNum], relT[imgNum], threshold = 0.01)                 
+    match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
+                        current_keypoints, matches, 0, 
+                        matchesMask =inlier_mask, #this applies your inlier filter
+                        flags=
+                        cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow('matches_after filter',match_visualization)
+    match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
                             current_keypoints, matches, 0, 
-                            matchesMask =inlier_mask, #this applies your inlier filter
                             flags=
                             cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-    else:
-        match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
-                                current_keypoints, matches, 0, 
-                                flags=
-                                cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 
     cv2.imshow('matches',match_visualization)
+
+    i = 0
+    while i < len(matches):
+        if feature_track[matches[i].queryIdx] < 3:
+            del matches[i]
+        i += 1
+    referencePoints = np.float32([reference_keypoints[m.queryIdx].pt \
+                                  for m in matches])
+    imagePoints = np.float32([current_keypoints[m.trainIdx].pt \
+                                  for m in matches])
+    inlier_mask = FilterByEpipolarConstraint(intrinsics, matches, referencePoints, imagePoints, relR[imgNum], relT[imgNum], threshold = 0.01)  
+    match_visualization = cv2.drawMatches(reference, reference_keypoints, current_frame,
+                        current_keypoints, matches, 0, 
+                        matchesMask =inlier_mask, #this applies your inlier filter
+                        flags=
+                        cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow('track>=4, matches_after filter',match_visualization)
+    M = np.zeros((3*len(matches), len(matches) + 1))
+    print(len(matches))
+    
+    fx = intrinsics[0][0]
+    fy = intrinsics[1][1]
+    cx = intrinsics[0][2]
+    cy = intrinsics[1][2]
+    points1_3 = np.zeros((imagePoints.shape[0], 3))
+    points2_3 = np.zeros((referencePoints.shape[0], 3))
+
+    your_pointCloud = np.zeros((len(matches), 3))
+
+    for i in range(imagePoints.shape[0]):
+        points1_3[i] = [(imagePoints[i][0] - cx)/fx, (imagePoints[i][1] - cy)/fy, 1]
+        points2_3[i] = [(referencePoints[i][0] - cx)/fx, (referencePoints[i][1] - cy)/fy, 1]
+
+    for i in range(len(matches)):
+
+        M[i*3][i] = np.cross(points1_3[i], relR[imgNum], axisa = 0, axisb = 0).dot(points2_3[i])[0]
+        M[i*3+1][i] = np.cross(points1_3[i], relR[imgNum], axisa = 0, axisb = 0).dot(points2_3[i])[1]
+        M[i*3+2][i] = np.cross(points1_3[i], relR[imgNum], axisa = 0, axisb = 0).dot(points2_3[i])[2]
+
+        M[i*3][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[0]
+        M[i*3+1][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[1]
+        M[i*3+1][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[2]
+    W,U,Vt = cv2.SVDecomp(M)  
+    depths = Vt[-1,:]/Vt[-1,-1]
+    print(points2_3)
+    for i in range(len(matches)):
+        your_pointCloud[i] = points2_3[i] * [1, 1, depths[i]]
+    print(your_pointCloud)
     imgNum += 1
     k = cv2.waitKey(1)
-    time.sleep(2)
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(your_pointCloud)
+    o3d.visualization.draw_geometries([pcd])
+
     if k == 27 or k==113:  #27, 113 are ascii for escape and q respectively
         #exit
         break
+    elif k == 32: #32 is ascii for space
+        continue
+    time.sleep(1)
