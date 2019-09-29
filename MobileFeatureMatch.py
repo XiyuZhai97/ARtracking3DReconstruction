17423b68f6b654be557b27bc954260094a222d0a
XiyuZhai <zxy4970@gmail.com>
2019-09-29 02:44:23 -0700

    still have bugs in depth compute

--- MobileFeatureMatch.py
+++ MobileFeatureMatch.py
@@ -185,9 +185,11 @@ for fname in images[1:]:
     your_pointCloud = np.zeros((len(matches), 3))
 
     for i in range(imagePoints.shape[0]):
-        points1_3[i] = [(imagePoints[i][0] - cx)/fx, (imagePoints[i][1] - cy)/fy, 1]
-        points2_3[i] = [(referencePoints[i][0] - cx)/fx, (referencePoints[i][1] - cy)/fy, 1]
-
+        # points1_3[i] = [(imagePoints[i][0] - cx)/fx, (imagePoints[i][1] - cy)/fy, 1]
+        # points2_3[i] = [(referencePoints[i][0] - cx)/fx, (referencePoints[i][1] - cy)/fy, 1]
+        
+        points1_3[i] = np.append(imagePoints[i], 1)
+        points2_3[i] = np.append(referencePoints[i], 1)
     for i in range(len(matches)):
 
         M[i*3][i] = np.cross(points1_3[i], relR[imgNum], axisa = 0, axisb = 0).dot(points2_3[i])[0]
@@ -197,9 +199,10 @@ for fname in images[1:]:
         M[i*3][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[0]
         M[i*3+1][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[1]
         M[i*3+1][len(matches)] = np.cross(points1_3[i], relT[imgNum], axisa = 0, axisb = 0)[2]
-    W,U,Vt = cv2.SVDecomp(M)  
+    print("M", M)
+    W,U,Vt = cv2.SVDecomp(M)
     depths = Vt[-1,:]/Vt[-1,-1]
-    print(points2_3)
+
     for i in range(len(matches)):
         your_pointCloud[i] = points2_3[i] * [1, 1, depths[i]]
     print(your_pointCloud)
